b53695b8a3a5c0c38df756c25efa1e5566fb0504
suraj1905 <surajsharma9378@gmail.com>
2024-04-17 14:31:03 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Assignment2{
+    public class Assignment2{
+        public static void Main(string[] args){
+            Console.WriteLine(Sum(10,2));
+            Swap(172,999);
+        }
+
+
+        public static int Sum(int  a, int b){
+            return a+b;
+        }
+
+        public static void Swap(int  a, int b){
+            b = a+b;
+            a = b-a;
+            b -= a;
+            Console.WriteLine("After swap a = {0} and b = {1}",a,b);
+
+        }
+    }
+}
